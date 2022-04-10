01686a99e1b5e935c665f617c3efc525cf0b45ad
alex-hobby <alexhobby98@gmail.com>
2022-04-10 15:28:05 -0400

    Reorganize files

--- src/filter.r
+++
@@ -1,4 +0,0 @@
-#SELECT RELVEANT FEATURES
-#exclude features as per analysis under feature selection using 
-imputedDF <- imputedDF %>%
-  dplyr::select(-precipProbability, -month, -off_ct, -roundabt_ct, -holiday, -date, -geometry,)
